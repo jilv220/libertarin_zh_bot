d3808dcae26bdde7b082077060efb5177fb853e4
jilv220 <hkfhfkcoc@protonmail.com>
2022-12-24 19:07:59 -0500

    install as system service instead

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ chmod a+w /opt/${pkg_name}/twitter_bot.db
 
 cp twitter-bot.service.example twitter-bot.service
 sed -i "s/PWD/${pkg_name}/g" twitter-bot.service
-cp twitter-bot.service /etc/systemd/user/twitter-bot.service
-cp twitter-bot.timer.example /etc/systemd/user/twitter-bot.timer
+cp twitter-bot.service /etc/systemd/system/twitter-bot.service
+cp twitter-bot.timer.example /etc/systemd/system/twitter-bot.timer
 
 rm twitter-bot.service
